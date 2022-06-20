650d7b3f81934ed3befa84aca3e3474775741f6c
Marek-MC <76727652+Marek-MC@users.noreply.github.com>
2022-06-20 13:18:17 -0500

    css stuff

--- pages/components/navigation.js
+++ pages/components/navigation.js
@@ -7,46 +7,36 @@ class Navigation extends Component {
   render() {
     return (
 
-      <div id='navigation' className={styles.navigation}>
-
-        
+      <div className={styles.navigation}>
 
         <nav>
 
-          <ul>
-
+          <Link href="/">
             <svg id='moon' xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-moon" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M14.53 10.53a7 7 0 0 1-9.058-9.058A7.003 7.003 0 0 0 8 15a7.002 7.002 0 0 0 6.53-4.47z" />
             </svg>
+          </Link>
+
+          <ul>
 
             <li>
-              <Link href="/">
-                <a>Origin</a>
-              </Link>
+              <Link href="/">Origin</Link>
             </li>
 
             <li>
-              <Link href="/home">
-                <a>Home</a>
-              </Link>
+              <Link href="/home">Home</Link>
             </li>
 
             <li>
-              <Link href="/vault">
-                <a>Vault</a>
-              </Link>
+              <Link href="/vault">Vault</Link>
             </li>
 
             <li>
-              <Link href="/projects">
-                <a>Projects</a>
-              </Link>
+              <Link href="/projects">Projects</Link>
             </li>
 
             <li>
-              <Link href="/extra">
-                <a>Extra</a>
-              </Link>
+              <Link href="/extra">Extra</Link>
             </li>
 
           </ul>
